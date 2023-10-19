82589ef55138016264880adbaf8ea9596d5a1035
wastenio silva <wastenio.silva@gmail.com>
2023-10-19 17:55:14 -0300

    Desafio completo

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,7 +15,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido 
             // *IMPLEMENTE AQUI*
             if (Suite.Capacidade >= hospedes.Count)
             {
